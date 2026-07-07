6fc1a61bfb777106d6e012eaec0f56b9bf9c99e7
Thomas A. <99028957+Silinia@users.noreply.github.com>
2026-07-07 23:27:40 +0200

    delete test infra

--- infra/docker/postgres/init/01-init.sql
+++
@@ -1,5 +0,0 @@
--- Optionnel : ce fichier ne s'exécute qu'au tout premier démarrage (volume vide).
--- En principe, c'est Doctrine qui crée le schéma via :
---   php bin/console doctrine:migrations:migrate
--- On se contente d'activer une extension utile.
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
